360fcc9518c5b616dc29e5bc1d14aa8130d89683
Erik Mota Galindo <kenshimota@gmail.com>
2022-01-20 20:11:45 -0400

    validate password

--- app/utils/hash-password.js
+++ app/utils/hash-password.js
@@ -4,9 +4,30 @@ class HashPassword {
   /**
    * Function that encrypte an string
    * @param {String} str
+   * @return {Boolean}
    */
-  constructor(str) {
+  setPassword(str) {
+    const valid = this.validatePassword(str);
     this.token = sha256(str);
+    return valid;
+  }
+
+  /**
+   * Function that valid password
+   * @param {String} str
+   * @return {Boolean}
+   */
+  validatePassword(str) {
+    if (str.length < 7)
+      throw new Error("the password must have minimun 7 characters");
+    if (!str.match(/[a-z]/g))
+      throw new Error("the password must have a lowercase letter");
+    if (!str.match(/[A-Z]/g))
+      throw new Error("the password must have an capital letter");
+    if (!str.match(/[0-9]/g))
+      throw new Error("the password must have a number");
+
+    return true;
   }
 
   /**
