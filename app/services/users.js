99bce69f183cb294425d9b07e575481831dd93ca
Erik Mota Galindo <kenshimota@gmail.com>
2022-01-20 22:40:29 -0400

    valid name, username and create user

--- app/services/users.js
+++ app/services/users.js
@@ -1,4 +1,6 @@
 const model = require("../models/users");
+const Emails = require("../utils/emails");
+const HashPassword = require("../utils/hash-password");
 
 class Users {
   /* ---- Construct ---- */
@@ -9,12 +11,82 @@ class Users {
   /**
    * Function that get an users for id
    * @param {Number} id
+   * @return {SequelizeModel}
    */
   async getId(id) {
     const resource = await this.model.findByPk(id);
     if (!resource) throw new Error("sorry, the users not exists");
-    this.resource = this.resource;
+    this.resource = this.resource.dataValues;
     return resource;
+  }
+
+  /**
+   * Function that valid username
+   * @param {String} str
+   * @return {Boolean}
+   */
+  async validUsername(str = "") {
+    if (str.length < 6 && str.length > 255)
+      throw new Error(
+        "sorry, the username must have more 6 and maximun 255 charaters"
+      );
+
+    const response = await this.model.findOne({ where: { username: str } });
+    if (!response)
+      throw new Error(
+        "sorry, the username  " + str + " exists, please intro other username"
+      );
+
+    return true;
+  }
+
+  validName(str) {
+    if (!str || str.length <= 1)
+      throw new Error(
+        "sorry, the user must an firsname and lastname with 2 characteres please"
+      );
+
+    if (str.match(/[0-9]/g))
+      throw new Error(
+        "sorry, the user must an firsname and lastname with 2 characteres please"
+      );
+
+    return true;
+  }
+
+  /**
+   * Function that create an user
+   * @param {String} username
+   * @param {String} email
+   * @param {String} firstname
+   * @param {String} lastname
+   * @param {String} email
+   * @param {String} password
+   * @return {SequelizeModel}
+   */
+  async create({ username, email, firstname, lastname, password }) {
+    const hash = new HashPassword();
+    hash.setPassword(password);
+
+    const emails = new Emails();
+    emails.setEmail(email);
+
+    this.validUsername(username);
+    this.validName(firstname);
+    this.validName(lastname);
+
+    const attributes = {
+      username,
+      firstname,
+      lastname,
+      email: emails.getEmail(),
+      password: hash.getToken(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    const response = await this.model.create(attributes);
+    return response;
   }
 
   /**
