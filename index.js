037d4c5081aced1c60790ace5db7d723df6d871c
Erik Mota Galindo <kenshimota@gmail.com>
2022-01-21 11:35:45 -0400

    add cors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const router = require("./app/routes/index");
 
+app.use(cors());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
