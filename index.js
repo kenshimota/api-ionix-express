a974a3729854a5c45de0e6f0dc432d1ffc9e5927
Erik Mota Galindo <kenshimota@gmail.com>
2022-01-21 03:41:41 -0400

    add routes to our server

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const port = process.env | 3000;
+const router = require("./app/routes/index");
+
+app.use("/", router);
 
 app.listen(port, () => {
   console.log(`the server started to port ${port}`);
