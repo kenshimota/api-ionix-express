bba9e85926c9d87a68487d7dd48db2bdaad006ec
Erik Mota Galindo <kenshimota@gmail.com>
2022-01-21 03:38:48 -0400

    add router for loggin

---
+++ app/routes/api/auth.js
@@ -0,0 +1,25 @@
+const { Router, response } = require("express");
+const Auth = require("../../services/auth");
+const router = Router();
+
+/**
+ * # POST /api/auth/login
+ * Function that return token valid
+ * @param {String} username
+ * @param {String} password
+ * @param {Object}
+ */
+router.post("auth/login", async function (req, res) {
+  try {
+    const auth = new Auth();
+    const { username, password } = req;
+    const token = await auth.signin({ username, password });
+
+    res.json({ token });
+  } catch (error) {
+    res.status(422);
+    res.json({ message: error.message });
+  }
+});
+
+module.exports = router;
