9c7131ae5867349d2ab707fa4d451e48f8375b17
Erik Mota Galindo <kenshimota@gmail.com>
2022-01-20 21:36:24 -0400

    class emails for valid emails

---
+++ app/utils/emails.js
@@ -0,0 +1,30 @@
+class Emails {
+  /**
+   * Function that set the email an string
+   * @param {String} str
+   * @return {Boolean}
+   */
+  setEmail(str) {
+    const valid = this.validate(str);
+    this.email = str;
+    return valid;
+  }
+
+  /**
+   * Function that check if string is valid
+   * @param {String} str
+   * @return {Boolean}
+   */
+  validate(str) {
+    const validRegex =
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (str.length == 0) throw new Error("the email can't is void");
+
+    const validResponse = str.match(validRegex);
+    if (!validResponse) throw new Error("the email not is valid");
+
+    return true;
+  }
+}
+
+module.exports = Emails;
