86ad235218dd8e43d60bd091e861c9478f905f53
Erik Mota Galindo <kenshimota@gmail.com>
2022-01-21 03:40:08 -0400

    create middlewares auth-headers

---
+++ app/middlewares/auth-headers.js
@@ -0,0 +1,27 @@
+const Auth = require("../services/auth");
+
+/**
+ * Function that check if there is a token and this is valid
+ * @param {RequestExpress} req
+ * @param {ResponseExpress} res
+ * @param {NextExpress} next
+ * @return {Void}
+ */
+const AuthHeaders = function (req, res, next) {
+  try {
+    if (!req.headers["authorization"])
+      throw new Error("sorry, invalid credentials");
+
+    const token = req.headers["authorization"];
+    const auth = new Auth();
+    const user = auth.checkToken(token);
+    req.currentUser = user;
+
+    return next();
+  } catch (error) {
+    res.status(401);
+    res.json({ message: error.message });
+  }
+};
+
+module.exports = AuthHeaders;
