eafa1c6100b4ea3c71331d156679f2518b95a50d
Erik Mota Galindo <kenshimota@gmail.com>
2022-01-21 07:34:22 -0400

    correcting callback on test to auth

--- test/auth.test.js
+++ test/auth.test.js
@@ -1,22 +1,28 @@
 const Auth = require("../app/services/auth");
 const userTest = { username: "panthon0", password: "8UFd7u75S" };
 
-test("into password invalid", () => {
+it("into password invalid", async () => {
   const auth = new Auth();
-  auth
-    .signin({ username: userTest.username, password: userTest.password + "1" })
-    .catch((e) => expect(e).toThrow(Error));
+  await expect(
+    auth.signin({
+      username: userTest.username,
+      password: userTest.password + "1",
+    })
+  ).rejects.toThrow(Error);
 });
 
-test("into password valid", () => {
+test("into password valid", (done) => {
   const auth = new Auth();
-  auth
-    .signin(userTest)
-    .then((token) => {
+
+  function callback(token) {
+    try {
       const user = auth.checkToken(token);
       expect(user.username).toBe(userTest.username);
-    })
-    .catch((e) => {
-      throw new Error(e);
-    });
+      done();
+    } catch (error) {
+      done(error);
+    }
+  }
+
+  auth.signin(userTest).then(callback);
 });
