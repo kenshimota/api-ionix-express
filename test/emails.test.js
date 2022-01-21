6b08386503e14245e62ea39e36dfdd2e99acf42b
Erik Mota Galindo <kenshimota@gmail.com>
2022-01-20 21:36:56 -0400

    test email

---
+++ test/emails.test.js
@@ -0,0 +1,16 @@
+const Emails = require("../app/utils/emails");
+
+test("email void", () => {
+  const emails = new Emails();
+  expect(() => emails.setEmail("")).toThrow(Error);
+});
+
+test("email wrong string", () => {
+  const emails = new Emails();
+  expect(() => emails.setEmail("holacomostas.com")).toThrow(Error);
+});
+
+test("email valid", () => {
+  const emails = new Emails();
+  expect(emails.setEmail("admin@example.com")).toBe(true);
+});
