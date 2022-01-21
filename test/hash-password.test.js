3d7e76e594b5df541e7b24f08d15a4f0f4a51234
Erik Mota Galindo <kenshimota@gmail.com>
2022-01-20 20:49:06 -0400

    unit test for hash-password

---
+++ test/hash-password.test.js
@@ -0,0 +1,26 @@
+const HashPassword = require("../app/utils/hash-password");
+
+test("password void", () => {
+  const hash = new HashPassword();
+  expect(() => hash.setPassword("")).toThrow(Error);
+});
+
+test("password with just lower letter", () => {
+  const hash = new HashPassword();
+  expect(() => hash.setPassword("manager")).toThrow(Error);
+});
+
+test("password with just capital letter", () => {
+  const hash = new HashPassword();
+  expect(() => hash.setPassword("MANAGER")).toThrow(Error);
+});
+
+test("password with just number", () => {
+  const hash = new HashPassword();
+  expect(() => hash.setPassword("1234567")).toThrow(Error);
+});
+
+test("password with number, lower letter and capital letter", () => {
+  const hash = new HashPassword();
+  expect(hash.setPassword("123ABCabc")).toBe(true);
+});
