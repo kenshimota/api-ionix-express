ee194c34e432ed5e65c7ca0b7d74e9ca2852e01b
Erik Mota Galindo <kenshimota@gmail.com>
2022-01-22 07:50:34 -0400

    add route me

--- app/routes/api.js
+++ app/routes/api.js
@@ -3,7 +3,9 @@ const router = Router();
 
 const users = require("./api/users");
 const auth = require("./api/auth");
+const me = require("./api/me");
 
 router.use("/users", users);
 router.use("/auth", auth);
+router.use("/me", me);
 module.exports = router;
